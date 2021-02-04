5617f6785cd74e57dbabcd830826083641e0a82f
syr0s <daniel.noetzel@gmail.com>
2021-02-04 12:00:43 +0100

    type exception in format function tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,13 @@
 const logging = require('../lib/index.js')
 const fs = require('fs')
 
-describe('Test getDateTime function', () => {
+describe('Test getDateTime() function', () => {
   test('getDateTime must return a string', () => {
     expect(logging.getDateTime()).toBeDefined
   });
 });
 
-describe('Test writeLog function', () => {
+describe('Test writeLog(msg) function', () => {
   test('Write logfile', () => {
     const msg = 'foo'
     logging.writeLog(msg, (response) => {
@@ -15,11 +15,27 @@ describe('Test writeLog function', () => {
     })
   })
 })
-describe('Test format function', () => {
+describe('Test format(level, service, msg) function', () => {
   test('Type exception parameter level not number', () => {
     const level = 'foo'
     const service = 'bar'
     const msg = 'buz'
+    expect(() => {
+      logging.format(level, service, msg)
+    }).toThrow
+  })
+  test('Type exception parameter service not string', () => {
+    const level = 1
+    const service = 999
+    const msg = 'foo'
+    expect(() => {
+      logging.format(level, service, msg)
+    }).toThrow
+  })
+  test('Type exception parameter msg not string', () => {
+    const level = 1
+    const service = 'foo'
+    const msg = 999
     expect(() => {
       logging.format(level, service, msg)
     }).toThrow
