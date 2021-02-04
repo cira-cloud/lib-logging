3294551501b3c6058f73281a39b0a16ad9865157
syr0s <daniel.noetzel@gmail.com>
2021-02-04 11:28:23 +0100

    writeLog(msg) function

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,17 @@
 const logging = require('../lib/index.js')
+const fs = require('fs')
 
 describe('Test getDateTime function', () => {
   test('getDateTime must return a string', () => {
     expect(logging.getDateTime()).toBeDefined
-  })
-})
+  });
+});
 
-describe('Test writeLog function', () = {
-  test('Update logfile', () => {
-    const msg = 'foobar'
-    expect(logging.writeLog(msg))
+describe('Test writeLog function', () => {
+  test('Write logfile', () => {
+    const msg = 'foo'
+    logging.writeLog(msg, (response) => {
+      expect(response).toBe(true)
+    })
   })
 })
