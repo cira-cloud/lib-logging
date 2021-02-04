881c17df64a3d19d032d88de8c0446c966d2d5ea
syr0s <daniel.noetzel@gmail.com>
2021-02-04 10:31:10 +0100

    getDateTime function

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,8 +4,4 @@ describe('Test getDateTime function', () => {
   test('getDateTime must return a string', () => {
     expect(logging.getDateTime()).toBeDefined
   })
-  test('getDateTime returns the current datetime string', () => {
-    const expected = new Date(Date.now)
-    expect(logging.getDateTime()).toBe(expected)
-  })
 })
